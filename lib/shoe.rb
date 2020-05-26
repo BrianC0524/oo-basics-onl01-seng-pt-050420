7e49947ac1438fb102633ffa431e107b30bb058b
Brian Craycraft <craycraftb24@hotmail.com>
2020-05-26 12:56:15 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,16 @@
 # Make your shoe class here!
+class Shoe
+  
+  attr_accessor :condition
+  attr_reader :brand, :color, :size, :material
+  
+  
+  def initialize(brand)
+    
+    @brand = brand
+    
+  end
+  
+  
+  
+end
